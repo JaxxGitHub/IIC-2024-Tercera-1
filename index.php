500da6c9ce10590e52a0fd9977ed0703a989303f
JaxxGitHub <jascollado8@gmail.com>
2024-06-17 23:16:01 -0400

    Semana 2

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once "./parte_superior.php" ?>
+
+<div class="tiutlo">
+    <p>Estudiar una carrera universitaria etc</p>
+</div>
+
+<body>
+    <div class="mmain-img">
+        <img src="./images/logo.png" alt="">
+    </div>
+</body>
+
+<?php require_once "./parte_inferior.php" ?>
--- parte_superior.php
+++ parte_superior.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 
 <head>
-<html lang="English">
-    <meta charset="utf-8"> 
+    <html lang="English">
+    <meta charset="utf-8">
     <meta name="Proyecto de Tercera 1" content="Pagina de Prueba">
     <title>
         Tercera 1
-</title>
-<link rel="stylesheet" href="./style/main.css">
-
+    </title>
+    <link rel="stylesheet" href="./style/main.css">
 </head>
-
 
 <menu>
     <div class="menu-izquierda">
-        <img src="./images/menu.png" alt="">
-        <p class="Texto-menu-izquierda">
-Bob esponja
-        </p>
-</div>
+        <a href="./index.php">
+            <img src="./images/logo.png" alt="">
+    </div>
+    <div class="menu-derecha">
+        <a class="enlaces-menu-derecha" href="https://google.com">Primer Ingreso</a>
+        <a class="enlaces-menu-derecha" href="https://google.com">Aula en linea</a>
+    </div>
 </menu>
